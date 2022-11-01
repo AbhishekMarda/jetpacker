306e292cd0d604b4a07449e2bae989920051c75e
AbhishekMarda <abhishek1marda@ucla.edu>
2022-10-31 17:31:40 -0700

    Initial commit

---
+++ jetpacker.js
@@ -0,0 +1,31 @@
+import {defs, tiny} from './examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+} = tiny;
+
+export class Jetpacker extends Scene {
+    constructor() {
+        // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
+        super();
+
+        // At the beginning of our program, load one of each of these shape definitions onto the GPU.
+        this.shapes = {
+        };
+
+        // *** Materials
+        this.materials = {
+
+        }
+
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+    }
+
+    make_control_panel() {
+
+    }
+
+    display(context, program_state) {
+
+    }
+}
