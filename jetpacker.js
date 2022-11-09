7f303e80e1a0e441eb5d3494d150eadbe442ebc9
AbhishekMarda <abhishek1marda@ucla.edu>
2022-11-09 13:56:22 -0800

    Adds a sphere in the middle

--- jetpacker.js
+++ jetpacker.js
@@ -11,21 +11,39 @@ export class Jetpacker extends Scene {
 
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
+            player : new defs.Subdivision_Sphere(4)
         };
 
         // *** Materials
         this.materials = {
+            player_material : new Material(new defs.Phong_Shader(),
+                {ambient: 0, diffusivity: 0.4, color: hex_color("#80FFFF")}),
+        };
 
-        }
-
-        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.initial_camera_location = Mat4.look_at(vec3(0, 1, 2), vec3(0, 0, 0), vec3(0, 1, 0));
     }
 
     make_control_panel() {
-
+        // this.key_triggered_button()
     }
 
     display(context, program_state) {
+        // display():  Called once per frame of animation. Here, the base class's display only does
+        // some initial setup.
 
+        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(Mat4.translation(-24.30, -6.01, -47.99));
+        }
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, 1, 100);
+
+        // *** Lights: *** Values of vector or point lights.
+        const light_position = vec4(0, 5, 5, 1);
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+        // program_state.set_camera(this.initial_camera_location);
+        this.shapes.player.draw(context, program_state, Mat4.scale(3,3,3), this.materials.player_material);
     }
 }
