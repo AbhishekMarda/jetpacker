7b891d776002d04c9a81a21fbd30f8253d0f8475
mich-xie <xiexmichelle@gmail.com>
2022-11-30 18:23:44 -0800

    Fix laser and background textures, add lighting, and updates score

--- jetpacker.js
+++ jetpacker.js
@@ -8,6 +8,8 @@ const {
 
 const {Textured_Phong} = defs
 
+const endpoint_scale = 1.5;
+
 class Laser {
     static shapes = {
         cylinder : new defs.Rounded_Capped_Cylinder(100, 100),
@@ -15,8 +17,9 @@ class Laser {
 
     static materials = {
         laser_material : new Material(new defs.Phong_Shader(),
-            {ambient: 0.6, color: hex_color("#FFA500")})
-    }
+            {ambient: 1, specularity: 1, diffusivity: 1, color: hex_color('#08FF08') })
+        };
+
     constructor(transx,transy,transz,scalex,scaley,scalez,rottheta) {
         this.transx = transx;
         this.transy = transy;
@@ -28,24 +31,41 @@ class Laser {
 
         this.rottheta = rottheta;
 
+        this.endpoint1_matrix = Mat4.identity();
+        this.endpoint2_matrix = Mat4.identity();
+        this.light_matrix = Mat4.identity();
     }
 
     draw(context, program_state, time_between_frames) {
-
         // align cylinder to the axis we need
         let model_transform = Mat4.rotation(Math.PI / 2, 1, 0 ,0);
+        this.light_matrix = Mat4.rotation(Math.PI / 2, 1, 0 ,0);
+        this.endpoint1_matrix = Mat4.rotation(Math.PI / 2, 1, 0 ,0);
+        this.endpoint2_matrix = Mat4.rotation(Math.PI / 2, 1, 0 ,0);
 
         // first move the object down so that the top end is at the origin
         model_transform = Mat4.translation(0, -0.5, 0).times(model_transform);
+        this.light_matrix = Mat4.translation(0, -this.scaley/(endpoint_scale * 2), 0).times(this.light_matrix);
+        this.endpoint1_matrix = Mat4.translation(0, -0.5, 0).times(this.endpoint1_matrix);
+        this.endpoint2_matrix = Mat4.translation(0, -this.scaley/endpoint_scale, 0).times(this.endpoint2_matrix);
 
         // then scale
         model_transform = Mat4.scale(this.scalex, this.scaley, this.scalez).times(model_transform);
+        this.light_matrix = Mat4.scale(endpoint_scale, endpoint_scale, endpoint_scale).times(this.light_matrix);
+        this.endpoint1_matrix = Mat4.scale(endpoint_scale, endpoint_scale, endpoint_scale).times(this.endpoint1_matrix);
+        this.endpoint2_matrix = Mat4.scale(endpoint_scale, endpoint_scale, endpoint_scale).times(this.endpoint2_matrix);
 
         // then rotate
         model_transform = Mat4.rotation(this.rottheta, 0, 0, 1).times(model_transform);
+        this.light_matrix = Mat4.rotation(this.rottheta, 0, 0, 1).times(this.light_matrix);
+        this.endpoint1_matrix = Mat4.rotation(this.rottheta, 0, 0, 1).times(this.endpoint1_matrix);
+        this.endpoint2_matrix = Mat4.rotation(this.rottheta, 0, 0, 1).times(this.endpoint2_matrix);
 
         // then move the endpoint to where we want
         model_transform = Mat4.translation(this.transx, this.transy, this.transz).times(model_transform);
+        this.light_matrix = Mat4.translation(this.transx, this.transy, this.transz).times(this.light_matrix);
+        this.endpoint1_matrix = Mat4.translation(this.transx, this.transy, this.transz).times(this.endpoint1_matrix);
+        this.endpoint2_matrix = Mat4.translation(this.transx, this.transy, this.transz).times(this.endpoint2_matrix);
 
         Laser.shapes.cylinder.draw(context, program_state, model_transform, Laser.materials.laser_material);
 
@@ -56,6 +76,14 @@ class Laser {
     getXPos() {
         return this.transx;
     }
+
+    getEndpoints() {
+        return [this.endpoint1_matrix, this.endpoint2_matrix];
+    }
+
+    getCenter() {
+        return this.light_matrix;
+    }
 }
 
 class Coin {
@@ -65,7 +93,7 @@ class Coin {
 
     static materials = {
         coin_material : new Material(new defs.Phong_Shader(),
-            {ambient: .9, diffusivity: 1, color: hex_color("#dcb82b")}),
+            {ambient: 1, diffusivity: 1, color: hex_color("#dcb82b")}),
     }
 
     constructor(transx, transy) {
@@ -117,7 +145,9 @@ export class Jetpacker extends Scene {
             background1 : new defs.Cube(),
             background2 : new defs.Cube(), 
             background3 : new defs.Cube(),
-            ground : new defs.Cube()
+            ground : new defs.Cube(), 
+            sphere1 : new defs.Subdivision_Sphere(4),
+            sphere2 : new defs.Subdivision_Sphere(4)
         };
 
         // *** Materials
@@ -145,25 +175,13 @@ export class Jetpacker extends Scene {
             background_material: new Material(new Texture_Scroll_X(), {
                 color: hex_color("#000000"),
                 ambient: 1, 
-                diffusivity: 0.1,
+                diffusivity: 1,
                 specularity: 0.1,
                 texture: new Texture("assets/background.png", "LINEAR_MIPMAP_LINEAR")
-            }),
-            paused_ground_material: new Material(new defs.Textured_Phong(), {
-                color: hex_color("#000000"),
-                ambient: 1, 
-                diffusivity: 0.1,
-                specularity: 0.1,
-                texture: new Texture("assets/ground.jpeg", "LINEAR_MIPMAP_LINEAR")
-            }),
-            paused_background_material: new Material(new defs.Textured_Phong(), {
-                color: hex_color("#000000"),
-                ambient: 1, 
-                diffusivity: 0.1,
-                specularity: 0.1,
-                texture: new Texture("assets/background.png", "LINEAR_MIPMAP_LINEAR")
-            }),
-            
+            }), 
+            endpoint_material : new Material(new defs.Phong_Shader(),
+                {ambient: 0.4, specularity:0, diffusivity: 0.4, color: hex_color('#BEBEBE') 
+            })      
         };
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 1, 2), vec3(0, 0, 0), vec3(0, 1, 0));
@@ -192,20 +210,31 @@ export class Jetpacker extends Scene {
 
         this.laser_arr = []; // queue of laser objects
         this.coin_arr = [];
+        this.lights = [];
 
         this.game_paused = false;
         this.hard_mode = false;
         this.collision_detected = false;
         this.change_player_texture = false;
 
-
         this.points = 0;
+
+        this.first_laser_drawn = false;
 
         this.keys_html = document.getElementById('keys');
         this.game_status_html = document.getElementById('game-status');
+
+
     }
 
     make_control_panel() {
+        this.control_panel.innerHTML += "Collect coins to earn points and avoid the lasers to protect your egg from cracking! <br>";
+        this.new_line();
+        this.new_line();
+        this.live_string(box => box.textContent = "Points: " + this.points);
+        this.new_line();
+        this.new_line();
+
         this.key_triggered_button("Reset", ["r"], () => {
             this.reset();
         })
@@ -214,20 +243,22 @@ export class Jetpacker extends Scene {
         })
         
         this.key_triggered_button("Pause game", ["p"], () => {
-            this.game_paused = !this.game_paused;
+            if(!this.game_over) {
+                this.game_paused = !this.game_paused;
+            }
         })
     
         this.key_triggered_button("Hard mode", ["m"], () => {
             this.hard_mode = !this.hard_mode;
-            console.log("Hard mode on");
         })
     }
 
     setLights(program_state) {
         // *** Lights: *** Values of vector or point lights.
-        const light_position1 = vec4(0, 100, 10, 1);
-        const light_position2 = vec4(0, -100, -10, 1);
-        program_state.lights = [new Light(light_position1, color(1, 1, 1, 1), 1000), new Light(light_position2, color(1, 1, 1, 1), 1000)];
+        const camera_light = this.initial_camera_location.times(vec4(0,0,0,1));
+        program_state.lights = [
+            new Light(camera_light, color(1, 0.647, 0, 1), 10000)
+        ];
 
     }
 
@@ -278,7 +309,7 @@ export class Jetpacker extends Scene {
         return false;
     }
 
-    generateLaser() {
+    generateLaser(program_state) {
 
         let draw_new_laser = (this.time_since_laser_drawn / this.max_time_between_laser) > Math.random();
 
@@ -287,7 +318,7 @@ export class Jetpacker extends Scene {
 
         if (draw_new_laser) {
             this.time_since_laser_drawn = 0;
-            const entity_y = Math.random() * (this.scene_max_y_coord - this.scene_min_y_coord) + this.scene_min_y_coord;
+            const entity_y = Math.random() * (this.scene_max_y_coord - this.scene_min_y_coord) + this.scene_min_y_coord + 7;
             const scale_x = 1;
             const scale_z = 1;
             const scale_y = Math.random() * (30-10) +  10; // scale between 5 and 20 length
@@ -296,6 +327,11 @@ export class Jetpacker extends Scene {
             let laser = new Laser(entities_start_x, entity_y, entity_z, scale_x, scale_y, scale_z, rottheta);
 
             this.laser_arr.push(laser);
+
+            // place light at laser midpoint
+            this.lights.push(new Light(laser.getCenter().times(vec4(0,0,0,1)), color(1, 1, 1, 1), 1000000));
+            program_state.lights.push(new Light(laser.getCenter().times(vec4(0,0,0,1)), color(1, 1, 1, 1), 1000000));
+            console.log(this.lights); 
 
         } else {
             this.time_since_laser_drawn += this.time_between_frames;
@@ -321,6 +357,7 @@ export class Jetpacker extends Scene {
         while (this.laser_arr.length > 0) {
             if (this.laser_arr[0].getXPos() <= -40) {       // let the laser go a little off-screen
                 this.laser_arr.splice(0, 1);
+                this.lights.splice(1, 1);
             } else {
                 break;
             }
@@ -338,9 +375,17 @@ export class Jetpacker extends Scene {
     }
 
     drawLasers(context, program_state, t) {
-        for(let laser of this.laser_arr) {
+        this.laser_arr.forEach((laser, index) => {
             laser.draw(context, program_state, t);
-        }
+
+            // draw endpoint spheres
+            this.shapes.sphere1.draw(context, program_state, laser.getEndpoints()[0], this.materials.endpoint_material);
+            this.shapes.sphere2.draw(context, program_state, laser.getEndpoints()[1], this.materials.endpoint_material);
+
+            // update light position
+            this.lights[index] = new Light(laser.getCenter().times(vec4(0,0,0,1)), color(1, 1, 1, 1), 1000000);
+            program_state.lights[index + 1] = this.lights[index];
+        })
     }
 
     drawCoins(context, program_state, t, time_between_frames) {
@@ -381,6 +426,14 @@ export class Jetpacker extends Scene {
         this.player_velocity = 0.0;
         this.game_paused = false;
         this.laser_arr = [];
+        this.coin_arr = [];
+        this.lights = [];
+        this.points = 0;
+    }
+
+    updateGamePaused(program_state)
+    {
+        program_state.animate = !this.game_paused;
     }
 
     display(context, program_state) {
@@ -389,60 +442,46 @@ export class Jetpacker extends Scene {
 
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
         if (!context.scratchpad.controls) {
-            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(Mat4.translation(-51.63, -18.34, -87.29));
+            this.initial_camera_location = Mat4.translation(-51.63, -18.34, -87.29);
+            program_state.set_camera(this.initial_camera_location);
+            program_state.game_paused = false;
+            this.setLights(program_state);
         }
 
+        this.updateGamePaused(program_state);
+
         if (this.hard_mode) {
-            let hard_mode_camera = Mat4.look_at(vec3(-3, -2.15, 15), vec3(0, 0, 0), vec3(0, 1, 0));
-            hard_mode_camera = hard_mode_camera.times(Mat4.translation(-21, -5, -57));
-            program_state.set_camera(hard_mode_camera);
+            this.initial_camera_location = Mat4.look_at(vec3(-3, -2.15, 15), vec3(0, 0, 0), vec3(0, 1, 0));
+            this.initial_camera_location = this.initial_camera_location.times(Mat4.translation(-21, -5, -57));
         }
         else {
-            program_state.set_camera(Mat4.translation(-51.63, -18.34, -87.29));
+            this.initial_camera_location = Mat4.translation(-51.63, -18.34, -87.29);
         }
+        program_state.set_camera(this.initial_camera_location);
 
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
 
-        this.setLights(program_state);
         this.drawPlayer(context, program_state);
 
         let t = program_state.animation_time / 150;
 
         let model_transform = Mat4.identity().times(Mat4.translation(13, 22, -15)).times(Mat4.scale(35, 35, 10));
-        if (!this.collisionDetected(context, program_state) && !this.game_paused) {
-            this.shapes.background1.draw(context, program_state, model_transform, this.materials.background_material);
-        } else {
-            this.shapes.background1.draw(context, program_state, model_transform, this.materials.paused_background_material);
-        }
+        this.shapes.background1.draw(context, program_state, model_transform, this.materials.background_material);
 
         model_transform = Mat4.identity().times(Mat4.translation(83, 22, -15)).times(Mat4.scale(35, 35, 10));
-        if (!this.collisionDetected(context, program_state) && !this.game_paused) {
-            this.shapes.background2.draw(context, program_state, model_transform, this.materials.background_material);
-        } else {
-            this.shapes.background2.draw(context, program_state, model_transform, this.materials.paused_background_material);
-        }
+        this.shapes.background2.draw(context, program_state, model_transform, this.materials.background_material);
 
         model_transform = Mat4.identity().times(Mat4.translation(153, 22, -15)).times(Mat4.scale(35, 35, 10));
-        if (!this.collisionDetected(context, program_state) && !this.game_paused) {
-            this.shapes.background3.draw(context, program_state, model_transform, this.materials.background_material);
-        } else {
-            this.shapes.background3.draw(context, program_state, model_transform, this.materials.paused_background_material);
-        }
-
-
+        this.shapes.background3.draw(context, program_state, model_transform, this.materials.background_material);
 
         model_transform = Mat4.identity().times(Mat4.rotation(Math.PI, 0, 1, 0)).times(Mat4.translation(5, -90, 0)).times(Mat4.scale(150, 75, 50));
-        if (!this.collisionDetected(context, program_state) && !this.game_paused) {
-            this.shapes.ground.draw(context, program_state, model_transform, this.materials.ground_material);
-        } else {
-            this.shapes.ground.draw(context, program_state, model_transform, this.materials.paused_ground_material);
-        }
+        this.shapes.ground.draw(context, program_state, model_transform, this.materials.ground_material);
 
-        if (!this.collisionDetected(context, program_state) && !this.game_paused && !this.game_over) {
-            this.generateLaser(); // see if a laser must be added
+        if (!this.collisionDetected(context, program_state) && !this.game_paused && !this.game_over) {            
+            this.generateLaser(program_state); // see if a laser must be added
             this.generateCoins();
             this.updatePlayerPosition();
             this.updateLasers();
@@ -452,10 +491,14 @@ export class Jetpacker extends Scene {
 
             this.drawLasers(context, program_state, this.time_between_frames);
             this.drawCoins(context, program_state, t, this.time_between_frames);
+
         } else if (this.collisionDetected(context, program_state) && !this.collision_detected) {
+            // 1st time a collision was detected
+            this.game_paused = true;
+            program_state.animate = !this.game_paused;
+
             this.drawLasers(context, program_state, 0);
             this.drawCoins(context, program_state, t, 0)
-            this.game_paused = true;
             this.collision_detected = true;
             this.game_over = true;
             this.change_player_texture = true;
